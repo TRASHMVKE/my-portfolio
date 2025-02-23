48d40216e14469b11f51f9e1959174ac77d706b5
Michael Rodriguez <michaelsmidrodriguez31@gmail.com>
2025-02-23 19:22:03 -0400

    Primer commit

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,6 @@ import tailwindcss from "@tailwindcss/vite";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: "/https://github.com/TRASHMVKE/my-portfolio/",
+  base: "/my-portfolio/",
 
 });
